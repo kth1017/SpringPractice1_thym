e4280cd27afc05e685afd5b4000c7b1b4057c483
kth <rnjsxo1017@gmail.com>
2022-06-18 18:01:21 +0900

    0618 학습

--- src/main/java/hello/thymeleafbasic/basic/BasicController.java
+++ src/main/java/hello/thymeleafbasic/basic/BasicController.java
@@ -1,11 +1,14 @@
 package hello.thymeleafbasic.basic;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -13,20 +16,20 @@ import java.util.HashMap;
 @RequestMapping("/basic")
 public class BasicController {
 
-    // 1
+
     @GetMapping("text-basic")
     public String textBasic(Model model) {
         model.addAttribute("data", "Hello");
         return "basic/text-basic";
     }
-    //2
+
     @GetMapping("text-unescaped")
     public String textUnescaped(Model model) {
         model.addAttribute("data", "<br>Hello<br>");
         return "basic/text-unescaped";
     }
 
-    //3
+
     @GetMapping("/variable")
     public String variable(Model model) {
         User userA = new User("userA", 10);
@@ -36,7 +39,7 @@ public class BasicController {
         list.add(userA);
         list.add(userB);
 
-        HashMap<String , User> map = new HashMap<>();
+        HashMap<String, User> map = new HashMap<>();
         map.put("userA", userA);
         map.put("userB", userB);
 
@@ -56,5 +59,50 @@ public class BasicController {
             Username = username;
             this.age = age;
         }
+    }
+
+    // 3 편의 객체들
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean {
+        public String hello(String data) {
+            return "Hello " + data;
+        }
+    }
+
+    // 4 유틸리티 객체
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "basic/date";
+    }
+
+    // 5 url 링크
+    @GetMapping("link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
+    // 6 리터럴
+    @GetMapping("/literal")
+    public String literal(Model model) {
+        model.addAttribute("data", "Spring!");
+        return "basic/literal";
+    }
+
+    // 7 연산
+    @GetMapping("/operation")
+    public String operation(Model model) {
+        model.addAttribute("nullData", null);
+        model.addAttribute("data", "Spring!");
+        return "basic/operation";
     }
 }
